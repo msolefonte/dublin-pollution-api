78ec331024109008ea55d1ec0c011f6cfed273d1
Marc Solé <msolefonte@gmail.com>
2021-12-02 12:38:32 +0000

    Add release 1.0.0

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import {MySQLClient} from './lib/MySQLClient';
+import cors from 'cors';
+import databaseConfig from './config/database.json';
+import express from 'express';
+import path from 'path';
+
+function main() {
+    const mySQLClient = new MySQLClient(databaseConfig);
+    const app = express();
+    const port = 8080;
+
+    app.use(cors({origin: true}));
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    function isAreaValid(params: Record<string, unknown>): boolean {
+        try {
+            // @ts-ignore
+            return [1, 2, 3, 4, 5, 6].includes(parseInt(params.areaId));
+        } catch (_) {
+            return false
+        }
+    }
+
+    app.get('/v1/health', (_, res) => {
+        res.sendStatus(200);
+    });
+
+    app.get('/v1/emissions/:areaId', async (req, res) => {
+        if (isAreaValid(req.params)) {
+            try {
+                res.status(200).send(await mySQLClient.getEmissions(req.params.areaId));
+            } catch (e) {
+                console.error(e);
+                res.sendStatus(500);
+            }
+        } else {
+            res.sendStatus(400);
+        }
+    });
+
+    app.get('/v1/traffic/:areaId', async (req, res) => {
+        if (isAreaValid(req.params)) {
+            try {
+                res.status(200).send(await mySQLClient.getTraffic(req.params.areaId));
+            } catch (e) {
+                console.error(e);
+                res.sendStatus(500);
+            }
+        } else {
+            res.sendStatus(400);
+        }
+    });
+
+    app.get('/v1/last-update', async (_, res) => {
+            try {
+                res.status(200).send(await mySQLClient.getLastUpdate());
+            } catch (e) {
+                console.error(e);
+                res.sendStatus(500);
+            }
+    });
+
+    app.get('/v1/data-points', async (_, res) => {
+            try {
+                res.status(200).send(await mySQLClient.getTotalDataPoints());
+            } catch (e) {
+                console.error(e);
+                res.sendStatus(500);
+            }
+    });
+
+    app.listen(port);
+}
+
+main();
